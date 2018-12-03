6ca3e9325ff89ed7900a002d68f3757c9157852d
sabasm <smma1992@gmail.com>
2018-12-03 13:06:44 -0600

    Facebook login done, still needs logout and cookie sessions

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const userSchema= new Schema({
 username:String,
-googleId:String
+googleId:String,
+facebookId:String
 })
 
 module.exports = mongoose.model('User',userSchema)
