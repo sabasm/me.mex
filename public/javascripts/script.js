107b5172b7c17efe48fba0ed0990f44c32398950
Gerardo <gera@MacBook-de-Gerardo.local>
2018-12-04 11:37:40 -0600

    Searchbar movement

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,8 +15,36 @@ document.getElementById('toggleClose').addEventListener('click', function(){
     document.getElementById("cover").style.zIndex = "-1"
 })
 
+document.getElementById('cover').addEventListener('click', function(){
+  document.getElementById("mySidenav").style.width = "0"
+  document.getElementById("mySidenav").style.opacity = "0"
+  document.getElementById("main").style.marginLeft= "0"
+  document.getElementById("cover").style.opacity = "0"
+  document.getElementById("cover").style.zIndex = "-1"
+})
+
 document.getElementById('searchButton').addEventListener('click', function(){
   document.getElementById('searchBar').removeAttribute('hidden')
   document.getElementById('searchButton').setAttribute('href','#')
   document.getElementById('searchButton').setAttribute('onclick',"document.getElementById('searchForm').submit()")
+  updatePh()
+  document.getElementById('main').style.marginTop = '86px'
+  document.getElementById('bodyHbs').style.height = 'calc(100vh - 130px)'
 })
+
+let i = 0
+let words = ['Memela...', 'KYC viejo lesbiano...', 'Channel...']
+
+function nextWord(){
+  i++
+  return words[i-1]
+}
+
+function updatePh(){
+  var intervalo = setInterval(function(){
+
+  document.getElementById('searchBar').setAttribute('placeholder', `${nextWord()}`)
+    if(i === 3) i = 0
+  },2000)
+}
+
