8b1d9ac45c6901ad2dae8d517586216822efaf6f
Gerardo <gera@MacBook-de-Gerardo.local>
2018-12-03 18:36:51 -0600

    Opacity almost done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,6 +2,9 @@ document.getElementById('toggleOpen').addEventListener('click', function(){
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("mySidenav").style.opacity = "1";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main").style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("nav").style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementsByClassName("items").style.backgroundColor = "rgba(0,0,0,0.4)";
 
 })
 
@@ -10,4 +13,7 @@ document.getElementById('toggleClose').addEventListener('click', function(){
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("mySidenav").style.opacity = "0";
     document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("main").style.backgroundColor = "white";
+    document.getElementById("nav").style.backgroundColor = "white";
+    document.getElementsByClassName("items").style.backgroundColor = "white";
 })
