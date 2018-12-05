4a4279a50d12df56614f2ee675a7b5ae0baea834
sabasm <smma1992@gmail.com>
2018-12-04 20:06:57 -0600

    editor de perfil agregado

--- routes/perfil.js
+++ routes/perfil.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const User = require('../models/user')
 
 const authCheck=(req,res,next)=>{
 if (!req.user){
@@ -10,6 +11,21 @@ res.redirect('/auth/login')
 
 router.get('/',authCheck,(req,res,next)=>{
   res.render('perfil',req.user)
+})
+
+//EDITOR DE PERFIL
+
+router.get('/edit',authCheck,(req,res,next)=>{
+  res.render('edit',req.user)
+})
+router.post('/edit',(req,res,next)=>{
+  let username = req.body.username
+  let email=req.body.email
+  let bio = req.body.bio
+
+  User.findByIdAndUpdate(req.user._id,{username,bio,email})
+  .then(
+    res.redirect('/perfil'))
 })
 
 module.exports=router
