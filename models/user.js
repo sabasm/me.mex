0dfad982fd3478907e140d61800f8d701024a994
sabasm <smma1992@gmail.com>
2018-12-04 19:26:21 -0600

    making profiles

--- models/user.js
+++ models/user.js
@@ -6,10 +6,17 @@ username:String,
 googleId:String,
 facebookId:String,
 email:String,
-bio:String,
-likedpost:{
-  type:String,
-  photoURL:[String]
+bio:{type:String
+,default:"Aquí va tu bio o mensaje personal"},
+canalesNumber:{type:Number
+  ,default:0},
+likes:{type:Number
+  ,default:0},
+  upvotes:{type:Number
+    ,default:0},
+likedPost:{
+  type:[String],
+  default:['https://www.catster.com/wp-content/uploads/2015/06/600px-outer-space-longcat-6ztyxR.jpg','https://i.kym-cdn.com/entries/icons/original/000/024/062/jerry.jpg','https://pbs.twimg.com/profile_images/2370446440/6e2jwf7ztbr5t1yjq4c5_400x400.jpeg']
 }
 })
 // -> set plugin into UserSchema
