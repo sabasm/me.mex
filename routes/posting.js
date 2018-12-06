e6c0a56574962f383583d1520de1500489f84904
Gerardo <gera@MacBook-de-Gerardo.local>
2018-12-06 17:08:43 -0600

    Upload fixed

--- routes/posting.js
+++ routes/posting.js
@@ -36,55 +36,41 @@ const authCheck=(req,res,next)=>{
   //   newCanal.save()
   // })
  
-
 router.post('/add',authCheck,(req,res,next)=>{
-url = req.body.url
-creatorId = req.user._id
+const url = req.body.url
+const creatorId = req.user._id
 
 const newPost = new Post({
 url,
 creatorId
 });
-newPost.save()
-.then(post=>{
-  //buscar el id del post por medio de su url anterior
-  Post.findOne({url: url}, function (error, post) {
-  })
-  .then(post=>{
-    // console.log('ENTRÉ AL THEN DE FIND ONE POST')
-    // console.log('ME TIRA ESTO: '+ post._id)
-    // console.log('se supone que mi usuario es : '+req.user.id)
-    User.findOneAndUpdate({"_id":req.user._id},{$push:{"likedPost":post.url}})
-    .then(res.render('posting/add',post))
 
-      
-    // console.log(req.user.likedPost)
-    // console.log(post)
-  })
-})
-.catch(err =>{
-  //console.log(err)
+newPost.save()
+.then(post => {
+  console.log(post)
+  User.findByIdAndUpdate({"_id":req.user._id},{$push:{"likedPost":post.url}})
+  .then(user => {
+    res.render('posting/add',post)
+    })
+  .catch(e => next(e))
+
 })
 });
 
-router.get('/add',authCheck,(req,res,next)=>{
-  res.send('editor after upload')
-  })
-
 router.post('/edit',authCheck,(req,res,next)=>{
-  console.log(req.body)
   let title = req.body.title
   let tags = req.body.tags
   let _id = req.body.id
 
-  Post.findOneAndUpdate({_id},{title,tags})
+  Post.findByIdAndUpdate(_id,{title,tags})
   .then(post=>{
     post.tags.forEach(tag => {
       Canal.findOne({title: tag}, function (error, canal) {
-      Canal.findByIdAndUpdate(canal._id, {$push: {"posts": post._id}}).then(res.redirect('/perfil'))
+      Canal.findByIdAndUpdate(canal._id, {$push: {"posts": post._id}})
       })
       
     });
+    res.redirect('/perfil')
   })
    
 })
