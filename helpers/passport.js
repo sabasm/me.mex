5b2af4ac9b84d46edfdc59bec396bb3f40472eb1
sabasm <smma1992@gmail.com>
2018-12-06 19:01:21 -0600

    Facebook 3

--- helpers/passport.js
+++ helpers/passport.js
@@ -69,7 +69,7 @@ passport.use(
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEAPPID,
     clientSecret: process.env.FACEAPPKEY,
-    callbackURL: "https://memecanales.herokuapp.com/auth/facebook/callback",
+    callbackURL: "https://memecanales.herokuapp.com/auth/callback/facebook",
     profileFields: ['id', 'displayName', 'photos', 'email']
   },
   function(accessToken, refreshToken, profile, done) {
