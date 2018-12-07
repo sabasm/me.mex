b709a6e9f324e6007b380fc60b1b3759e7b02a95
sabasm <smma1992@gmail.com>
2018-12-06 22:27:19 -0600

    got sharing on everything but profile

--- routes/trending.js
+++ routes/trending.js
@@ -4,6 +4,7 @@ const Canales = require('../models/canales')
 let logged 
 
 router.get('/',(req,res,next)=>{
+  console.log(req)
   var yesterday = Date.now()- 1000*60*60*24;
   Post.find({createdAt:{$gt:yesterday}}).sort({upvotes: -1}).limit(20)
   .then(posts=>
