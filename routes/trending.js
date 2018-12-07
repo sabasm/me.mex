8a7269b277555acf317f73383ac9556e56559ddf
sabasm <smma1992@gmail.com>
2018-12-06 18:06:08 -0600

    Trending done

--- routes/trending.js
+++ routes/trending.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const Post = require('../models/post')
+const Canales = require('../models/canales')
+let logged 
 
 router.get('/',(req,res,next)=>{
-  res.render('trending')
-})
+  var yesterday = Date.now()- 1000*60*60*24;
+  Post.find({createdAt:{$gt:yesterday}}).sort({upvotes: -1}).limit(20)
+  .then(posts=>
+res.render('canales/canal',{posts})
+  )
+});
+
+
 
 module.exports=router
